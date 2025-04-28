a04a0b2a9292fe5669916ed9f899b87e9fafc1c7
lamprose <29279979+lamprose@users.noreply.github.com>
2025-04-28 10:28:45 +0800

    add awake more option

--- src/memes/awake.rs
+++ src/memes/awake.rs
@@ -28,12 +28,18 @@ fn awake(images: Vec<InputImage>, texts: Vec<String>, _: NoOptions) -> Result<Ve
     ];
 
     let func = |i: usize, images: Vec<Image>| {
+        let pic_type = if images.len() == 2 { "" } else { "1" };
         let (x, y, d) = locs[i];
         let logo = images[0].round_corner(5.0).resize_exact((56, 56));
         let head = images[1].circle().resize_exact((51, 51)).rotate(d);
-        let frame = load_image(format!("awake/{i}.png"))?;
+        let frame = load_image(format!("awake/{pic_type}{i}.png"))?;
         let mut surface = new_surface(frame.dimensions());
         let canvas = surface.canvas();
+        if (images.len() == 3) {
+            let head2 = images[2].circle().resize_exact((83, 83));
+            canvas.clear(Color::WHITE);
+            canvas.draw_image(&head2, (116, 4), None)
+        }
         canvas.draw_image(&frame, (0, 0), None);
         canvas.draw_image(&logo, (3, 3), None);
         canvas.draw_image(&head, (x, y), None);
@@ -63,11 +69,11 @@ register_meme!(
     "awake",
     awake,
     min_images = 2,
-    max_images = 2,
+    max_images = 3,
     min_texts = 0,
     max_texts = 1,
     default_texts = &[DEFAULT_TEXT],
     keywords = &["醒醒"],
     date_created = local_date(2025, 4, 27),
-    date_modified = local_date(2025, 4, 27),
+    date_modified = local_date(2025, 4, 28),
 );
