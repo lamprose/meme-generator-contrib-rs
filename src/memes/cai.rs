c9f9ffbcbd8774174ce6d591682b0b5cedb4c6f3
lamprose <29279979+lamprose@users.noreply.github.com>
2025-04-29 10:54:14 +0800

    fix cai bugs

--- src/memes/cai.rs
+++ src/memes/cai.rs
@@ -15,7 +15,7 @@ use crate::{options::NoOptions, register_meme};
 const DEFAULT_TEXT_1: &str = "菜就哭呀";
 const DEFAULT_TEXT_2: &str = "练有几把用";
 fn cai(images: Vec<InputImage>, texts: Vec<String>, _: NoOptions) -> Result<Vec<u8>, Error> {
-    let frame = load_image("cai/0.jpg")?;
+    let frame = load_image("cai/0.png")?;
     let text1 = if texts.len() > 0 {
         &texts[0]
     } else {
@@ -31,8 +31,8 @@ fn cai(images: Vec<InputImage>, texts: Vec<String>, _: NoOptions) -> Result<Vec<
         let mut surface = frame.to_surface();
         let canvas = surface.canvas();
         canvas.clear(Color::WHITE);
-        let first_image = images[0].resize_exact((50, 50));
-        canvas.draw_image(&first_image, (36, 22), None);
+        let first_image = images[0].resize_exact((75, 75));
+        canvas.draw_image(&first_image, (23, 8), None);
         canvas.draw_image(&frame, (0, 0), None);
         canvas.draw_text_area_auto_font_size(
             IRect::from_ltrb(0, 77, 128, 93),
